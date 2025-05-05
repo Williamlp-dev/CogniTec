e27ae983b73dee7b1e881d56cc3a2ed4791fd79d
william.dev <williamlp.dev@gmail.com>
2025-05-05 18:43:13 -0300

    feat: Create Home component with background image and content layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
-import Image from 'next/image'
+import Home from '@/components/pages/Home'
 
-export default function Home() {
+export default function Pages() {
   return (
-    <main className="min-h-screen relative">
-      <div className="absolute inset-0">
-        <Image
-          src="/assets/img/cabeca3.png"
-          alt="Background cerebral"
-          fill
-          priority
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'center',
-            mixBlendMode: 'overlay',
-            filter: 'brightness(0.3)',
-          }}
-          className="z-0"
-        />
-      </div>
-
-      <div className="relative flex flex-col items-center justify-center h-screen text-center px-4 z-20">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6">
-          <span className="block mb-2">Cognitec</span>
-          <span className="block">
-            Soluções de IA para Alzheimer
-            <span className="text-amber-300">.</span>
-          </span>
-        </h1>
-      </div>
+    <main>
+      <Home />
     </main>
   )
 }
