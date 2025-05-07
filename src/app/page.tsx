ae50187e71895fd95dc4288577f5ddd9edd27962
Ib100x <ibsenfernando80@gmail.com>
2025-05-07 15:33:45 -0300

    feat: Added "Service" component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from '@/components/pages/Home'
+import Service from '@/components/pages/Service'
 
 export default function Pages() {
   return (
     <main>
       <Home />
+      <Service />
     </main>
   )
 }
