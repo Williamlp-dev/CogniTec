60fa2a4e085d1a377137f35e9a1d6d079ad212ae
william.dev <williamlp.dev@gmail.com>
2025-05-07 21:05:59 -0300

    feat: Update Pages component to include QA, TestimonialCarousel, Contact, and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import About from '@/components/pages/About'
+import TestimonialCarousel from '@/components/pages/Comments'
+import Contact from '@/components/pages/Contact'
+import { Footer } from '@/components/pages/Footer'
 import Home from '@/components/pages/Home'
+import QA from '@/components/pages/QA'
 import Service from '@/components/pages/Service'
 
 export default function Pages() {
@@ -8,6 +12,10 @@ export default function Pages() {
       <Home />
       <About />
       <Service />
+      <QA />
+      <TestimonialCarousel />
+      <Contact />
+      <Footer />
     </main>
   )
 }
