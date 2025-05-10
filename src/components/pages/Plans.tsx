c4f4f71090dd6b340f47be60ef02d8dbb4547222
Ib100x <ibsenfernando80@gmail.com>
2025-05-10 19:04:19 -0300

    feat: The cards were created, I used the "poppins" font and changed the colors.

--- src/components/pages/Plans.tsx
+++ src/components/pages/Plans.tsx
@@ -5,21 +5,27 @@ export default function Plans() {
   return (
     <section className="py-16 md:py-20 overflow-hidden">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 mb-12">
-        <TitleSection subtitle="Serviços" title="Conheça Nossos Serviços" />
+        <TitleSection subtitle="Serviços" title="Conheça Nossos Planos" />
       </div>
 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
           {/* Plano Gratuito */}
-          <div className="rounded-xs shadow-md p-6 flex flex-col h-full">
-            <div className="text-xl font-bold mb-2">Plano Gratuito</div>
+          <div className="bg-stone-100 rounded-xs shadow-md p-8 flex flex-col h-full">
+            <div className="text-xl font-poppins mb-2">Plano Gratuito</div>
             <div className="text-gray-700 mb-4">
               Ideal para familiares que desejam experimentar as funcionalidades
               básicas do app.
             </div>
-            <div className="text-3xl font-bold mb-2">R$0 / mês</div>
+            <div className="flex text-amber-400 py-5">
+              <div className="relative align-baseline text-xl font-bold">
+                R$
+              </div>
+              <div className="text-5xl font-bold">0</div>
+              <div className="text-gray-300 ml-1 mt-2 text-lg">/ mês</div>
+            </div>
             <a
-              className="w-full bg-white border border-gray-300 text-gray-700 text-center font-medium py-3 px-4 rounded mb-2 hover:bg-yellow-400 transition-colors"
+              className="w-full bg-stone-100 border border-gray-700 text-gray-500 text-center font-poppins py-3 px-4 rounded mb-2 hover:bg-yellow-400 transition-colors"
               href="#gratuito"
             >
               Comece Gratuitamente
@@ -63,18 +69,24 @@ export default function Plans() {
           </div>
 
           {/* Plano Profissional */}
-          <div className="rounded-3xl shadow-md p-6 bg-yellow-50 flex flex-col h-full relative">
-            <div className="absolute -top-4 right-6 bg-yellow-500 text-white px-4 py-1 rounded-full text-sm font-bold">
+          <div className="rounded-xs shadow-md p-6 bg-stone-100 flex flex-col h-full relative">
+            <div className="absolute -top-4 right-6 bg-yellow-500 text-black px-4 py-1 rounded-xs text-sm font-bold">
               Popular
             </div>
-            <div className="text-xl font-bold mb-2">Plano Profissional</div>
+            <div className="text-xl font-poppins mb-2">Plano Profissional</div>
             <div className="text-gray-700 mb-4">
               Perfeito para cuidadores que precisam de recursos avançados de
               monitoramento.
             </div>
-            <div className="text-3xl font-bold  mb-2">R$29,90 / mês</div>
+            <div className="flex text-amber-400 py-5">
+              <div className="relative align-baseline text-xl font-bold">
+                R$
+              </div>
+              <div className="text-5xl font-bold">39</div>
+              <div className="text-gray-300 ml-1 mt-2 text-lg">/ mês</div>
+            </div>
             <a
-              className="w-full bg-yellow-500 text-white text-center font-medium py-3 px-4 rounded mb-2 hover:bg-yellow-600 transition-colors"
+              className="w-full bg-yellow-500 text-gray-500 text-center font-poppins py-3 px-4 rounded mb-2 hover:bg-yellow-600 transition-colors"
               href="#profissional"
             >
               Assinar Agora
@@ -114,15 +126,21 @@ export default function Plans() {
           </div>
 
           {/* Plano Clínico */}
-          <div className="rounded-3xl shadow-md p-6 flex flex-col h-full">
-            <div className="text-xl font-bold mb-2">Plano Clínico</div>
+          <div className="bg-stone-100 rounded-xs shadow-md p-6 flex flex-col h-full">
+            <div className="text-xl font-poppins mb-2">Plano Clínico</div>
             <div className="text-gray-700 mb-4">
               Desenvolvido para clínicas e profissionais de saúde com múltiplos
               pacientes.
             </div>
-            <div className="text-3xl font-bold mb-2">R$79,90 / mês</div>
+            <div className="flex text-amber-400 py-5">
+              <div className="relative align-baseline text-xl font-bold">
+                R$
+              </div>
+              <div className="text-5xl font-bold">99</div>
+              <div className="text-gray-300 ml-1 mt-2 text-lg">/ mês</div>
+            </div>
             <a
-              className="w-full bg-white border border-gray-300 text-gray-700 text-center font-medium py-3 px-4 rounded mb-2 hover:bg-yellow-400 transition-colors"
+              className="w-full bg-stone-100 border border-gray-700 text-gray-500 text-center font-poppins py-3 px-4 rounded mb-2 hover:bg-yellow-400 transition-colors"
               href="#clinico"
             >
               Fale com um Consultor
