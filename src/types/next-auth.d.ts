2305ab0ead6775eac819d7c427e80ab0fc605914
william.dev <williamlp.dev@gmail.com>
2025-05-13 21:47:27 -0300

    feat: extend User, Session, and JWT types to include isAdmin property

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,30 +3,33 @@ import type { DefaultSession, DefaultUser } from "next-auth"
 
 declare module "next-auth" {
   /**
-   * Estendendo o tipo User para incluir userType
+   * Estendendo o tipo User para incluir userType e isAdmin
    */
   interface User extends DefaultUser {
     userType?: string
+    isAdmin?: boolean
     id: string
   }
 
   /**
-   * Estendendo o tipo Session para incluir userType no objeto user
+   * Estendendo o tipo Session para incluir userType e isAdmin no objeto user
    */
   interface Session {
     user: {
       id: string
       userType?: string
+      isAdmin?: boolean
     } & DefaultSession["user"]
   }
 }
 
 declare module "next-auth/jwt" {
   /**
-   * Estendendo o tipo JWT para incluir userType
+   * Estendendo o tipo JWT para incluir userType e isAdmin
    */
   interface JWT {
     userType?: string
+    isAdmin?: boolean
     id: string
   }
 }
